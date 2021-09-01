b9dd2ddfefae21b827f7edc5dcfb332db82e79ee
Alexey Rusakov <rusal@bk.ru>
2021-09-01 06:44:46 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace test4BISO
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Beep(5000, 1000);
+      Console.WriteLine("Hello BISO");
+    }
+  }
+}
